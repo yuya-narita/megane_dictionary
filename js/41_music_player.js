08f6cdb0d21890d9ec5eccd0084cea4b10bcb6c0
yuya-narita <yuya.narita.research@gmail.com>
2026-07-16 04:05:56 +0900

    Delete js directory

--- js/41_music_player.js
+++
@@ -1,3 +0,0 @@
-/* 41_music_player.js disabled
-   New playlist player is handled by 50_music_playlist_mode.js.
-*/
--- js/49_restore_nonmusic_layout.js
+++
@@ -1,1 +0,0 @@
-/* disabled by NAV_REAL_FIX: 49_restore_nonmusic_layout.js conflicted with unified bottom nav */
