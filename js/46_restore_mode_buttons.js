1ae69e2563e7a1f9bbf34d2466ad233466763ad8
yuya-narita <yuya.narita.research@gmail.com>
2026-07-17 01:55:43 +0900

    Delete js directory

--- js/46_restore_mode_buttons.js
+++
@@ -1,1 +0,0 @@
-/* disabled by NAV_REAL_FIX: 46_restore_mode_buttons.js conflicted with unified bottom nav */
--- js/55_emergency_stop.js
+++
@@ -1,1 +0,0 @@
-/* disabled in I: old bottom-nav handler caused forced dictionary jump / stop-only bug. */
